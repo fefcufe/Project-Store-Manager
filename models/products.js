cac9b0d1fa1ae8eeb6e77283689d774037246577
Fernanda Ferreira <fefcufe@gmail.com>
2022-07-04 13:39:51 -0300

    fix: erros de lint

--- models/products.js
+++ models/products.js
@@ -1,28 +1,27 @@
-const connection = require('../models/connection');
+const connection = require('./connection');
 
 const getAllProducts = async () => {
   const query = 'SELECT * FROM StoreManager.products;';
   const [dataProducts] = await connection.execute(query);
   return dataProducts;
-}
+};
 
 const getProductByIdModel = async (id) => {
   const query = 'SELECT * FROM StoreManager.products WHERE id = ?;';
   const [[data]] = await connection.execute(query, [id]);
   return data;
-}
+};
 
 const addProductModel = async (data) => {
-  const query =
-    `INSERT INTO StoreManager.products (name)
+  const query = `INSERT INTO StoreManager.products (name)
   VALUES (?);
   `;
   const [{ insertId }] = await connection.execute(query, [data.name]);
   return insertId;
-}
+};
 
 module.exports = {
   getAllProducts,
   getProductByIdModel,
   addProductModel,
-}
+};
